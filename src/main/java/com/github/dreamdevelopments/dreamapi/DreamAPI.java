b60ee55a1df29622f458836213122fdd0ca3b2d7
Styro457 <raduursache6@gmail.com>
2023-10-19 22:51:38 +0300

    added initializeGui in main initialize function

--- src/main/java/com/github/dreamdevelopments/dreamapi/DreamAPI.java
+++ src/main/java/com/github/dreamdevelopments/dreamapi/DreamAPI.java
@@ -2,6 +2,7 @@ package com.github.dreamdevelopments.dreamapi;
 
 import com.github.dreamdevelopments.dreamapi.ui.GuiManager;
 import lombok.Getter;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DreamAPI {
@@ -10,7 +11,7 @@ public final class DreamAPI {
     static ServerType serverType;
 
     public void initialize(JavaPlugin plugin) {
-        DreamAPI.serverType = serverType;
+        this.initializeGui(plugin);
     }
 
     public void initializeGui(JavaPlugin plugin) {
