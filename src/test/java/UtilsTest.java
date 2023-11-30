2f29668a54e1cfed95f7fbe3e22771cfe8555de8
Styro457 <raduursache6@gmail.com>
2023-11-30 16:36:31 +0200

    Create textTestUtils test

--- src/test/java/UtilsTest.java
+++ src/test/java/UtilsTest.java
@@ -7,4 +7,25 @@ import org.junit.jupiter.api.Test;
 
 
 public class UtilsTest {
+
+    @Test
+    public void testTextUtils() {
+        String formattedEnumName = TextUtils.formatEnumName("TEST_eNuM_Name", true);
+        Assertions.assertEquals("TestEnumName", formattedEnumName);
+
+        formattedEnumName = TextUtils.formatEnumName("TEST_eNuM_Name", false);
+        Assertions.assertEquals("testEnumName", formattedEnumName);
+
+        String formattedTime = TextUtils.formatTime(138);
+        Assertions.assertEquals("02:18", formattedTime);
+
+        formattedTime = TextUtils.formatTime(0);
+        Assertions.assertEquals("00:00", formattedTime);
+
+        formattedTime = TextUtils.formatTime(60);
+        Assertions.assertEquals("01:00", formattedTime);
+
+        formattedTime = TextUtils.formatTime(3600);
+        Assertions.assertEquals("60:00", formattedTime);
+    }
 }
