b574ffd182f0b330da6819924a48044a9d9bd5f3
Styro457 <raduursache6@gmail.com>
2023-11-30 16:36:09 +0200

    Create UtilsTest class

---
+++ src/test/java/UtilsTest.java
@@ -0,0 +1,10 @@
+import com.github.dreamdevelopments.dreamapi.messages.utils.TextConverter;
+import com.github.dreamdevelopments.dreamapi.utils.FontUtils;
+import com.github.dreamdevelopments.dreamapi.utils.TextUtils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class UtilsTest {
+}
