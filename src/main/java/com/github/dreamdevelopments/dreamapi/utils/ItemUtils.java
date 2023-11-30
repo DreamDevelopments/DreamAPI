689871284e06c850077edb244b6549928344fa46
Styro457 <raduursache6@gmail.com>
2023-11-30 17:24:59 +0200

    Create getItemLore function

--- src/main/java/com/github/dreamdevelopments/dreamapi/utils/ItemUtils.java
+++ src/main/java/com/github/dreamdevelopments/dreamapi/utils/ItemUtils.java
@@ -1,4 +1,45 @@
 package com.github.dreamdevelopments.dreamapi.utils;
 
+import com.github.dreamdevelopments.dreamapi.DreamAPI;
+import com.github.dreamdevelopments.dreamapi.messages.Message;
+import com.github.dreamdevelopments.dreamapi.messages.types.LegacyMessage;
+import com.github.dreamdevelopments.dreamapi.messages.types.ModernMessage;
+import net.kyori.adventure.text.Component;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ItemUtils {
+
+    @NotNull
+    public static List<Message> getItemLore(@NotNull ItemMeta meta) {
+        if(DreamAPI.getServerType().isModern())
+            return getLegacyItemLore(meta);
+        return getModernItemLore(meta);
+    }
+
+    @NotNull @SuppressWarnings("deprecation")
+    private static List<Message> getLegacyItemLore(@NotNull ItemMeta meta) {
+        List<Message> newLore = new ArrayList<>();
+        List<String> lore = meta.getLore();
+        if(lore == null)
+            return newLore;
+        for(String line : lore)
+            newLore.add(new LegacyMessage(line, false));
+        return newLore;
+    }
+
+    @NotNull
+    private static List<Message> getModernItemLore(@NotNull ItemMeta meta) {
+        List<Message> newLore = new ArrayList<>();
+        List<Component> lore = meta.lore();
+        if(lore == null)
+            return newLore;
+        for(Component line : lore)
+            newLore.add(new ModernMessage(line));
+        return newLore;
+    }
+
 }
