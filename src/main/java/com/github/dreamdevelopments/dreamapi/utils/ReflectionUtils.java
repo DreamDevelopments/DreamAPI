9590a340799526c30830f7923355f32e81419566
Styro457 <raduursache6@gmail.com>
2023-12-29 19:28:21 +0200

    Create getAllFields function

---
+++ src/main/java/com/github/dreamdevelopments/dreamapi/utils/ReflectionUtils.java
@@ -0,0 +1,30 @@
+package com.github.dreamdevelopments.dreamapi.utils;
+
+import com.google.common.collect.Lists;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+public class ReflectionUtils {
+
+    /**
+     * Returns all the fields from a class including those inherited
+     * @param clazz The class
+     * @return List of fields
+     */
+    public static Iterable<Field> getAllFields(@NotNull Class<?> clazz) {
+
+        List<Field> currentClassFields = Lists.newArrayList(clazz.getDeclaredFields());
+        Class<?> parentClass = clazz.getSuperclass();
+
+        if (parentClass != null) {
+            List<Field> parentClassFields =
+                    (List<Field>) getAllFields(parentClass);
+            currentClassFields.addAll(parentClassFields);
+        }
+
+        return currentClassFields;
+    }
+
+}
