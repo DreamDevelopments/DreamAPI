88164000c1814382c526e2aac3ab2ef49cb75005
Styro457 <raduursache6@gmail.com>
2023-12-29 05:56:11 +0200

    Enhance formatEnumName function

--- src/main/java/com/github/dreamdevelopments/dreamapi/utils/TextUtils.java
+++ src/main/java/com/github/dreamdevelopments/dreamapi/utils/TextUtils.java
@@ -7,11 +7,16 @@ public class TextUtils {
 
     @NotNull
     public static String formatEnumName(@NotNull String enumName, boolean capitalize) {
+        return formatEnumName(enumName, "", capitalize);
+    }
+
+    @NotNull
+    public static String formatEnumName(@NotNull String enumName, String wordDivider, boolean capitalize) {
         char[] name = enumName.toLowerCase().toCharArray();
         StringBuilder newString = new StringBuilder();
         newString.append(capitalize ? Character.toUpperCase(name[0]) : name[0]);
         for(int i = 1; i < name.length; i++)
-            newString.append(name[i] == '_' ? Character.toUpperCase(name[++i]) : name[i]);
+            newString.append(name[i] == '_' ? wordDivider + Character.toUpperCase(name[++i]) : name[i]);
         return newString.toString();
     }
 
