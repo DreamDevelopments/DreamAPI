9da66a22bada5f539118f5832ad9b88dd8341cbf
Styro457 <raduursache6@gmail.com>
2024-03-10 13:31:33 +0200

    Create streamToString function

--- src/main/java/com/github/dreamdevelopments/dreamapi/utils/StringUtils.java
+++ src/main/java/com/github/dreamdevelopments/dreamapi/utils/StringUtils.java
@@ -7,4 +7,26 @@ import java.io.InputStreamReader;
 
 public class StringUtils {
 
+    private static String streamToString(InputStream is) {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+
 }
