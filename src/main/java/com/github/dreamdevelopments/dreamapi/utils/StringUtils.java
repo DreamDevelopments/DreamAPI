d036f933f334d725c65b8b8f373badc98b32a32d
Styro457 <raduursache6@gmail.com>
2024-03-10 13:31:16 +0200

    Create StringUtils

---
+++ src/main/java/com/github/dreamdevelopments/dreamapi/utils/StringUtils.java
@@ -0,0 +1,10 @@
+package com.github.dreamdevelopments.dreamapi.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class StringUtils {
+
+}
