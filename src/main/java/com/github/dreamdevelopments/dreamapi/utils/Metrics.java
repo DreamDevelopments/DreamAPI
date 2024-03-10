5a57045f2b264d574e0ea75fd6dd795c56969a8c
Styro457 <raduursache6@gmail.com>
2024-03-10 13:32:16 +0200

    Create sendRequest function

--- src/main/java/com/github/dreamdevelopments/dreamapi/utils/Metrics.java
+++ src/main/java/com/github/dreamdevelopments/dreamapi/utils/Metrics.java
@@ -1,5 +1,54 @@
 package com.github.dreamdevelopments.dreamapi.utils;
 
+import org.bukkit.Bukkit;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Metrics {
+
+    public HttpResponse<String> sendRequest(@NotNull String url, @NotNull RequestType requestType) throws IOException, InterruptedException {
+        return sendRequest(url, requestType, null);
+    }
+
+    public HttpResponse<String> sendRequest(@NotNull String url, @NotNull RequestType requestType, @Nullable HashMap<String, String> arguments) throws IOException, InterruptedException {
+        String requestBody = "";
+        if(arguments != null && !arguments.isEmpty()) {
+            StringBuilder requestBodyBuilder = new StringBuilder();
+            for (Map.Entry<String, String> entry : arguments.entrySet()) {
+                requestBodyBuilder.append(URLEncoder.encode(entry.getKey(), StandardCharsets.UTF_8));
+                requestBodyBuilder.append("=");
+                requestBodyBuilder.append(URLEncoder.encode(entry.getValue(), StandardCharsets.UTF_8));
+                requestBodyBuilder.append("&");
+            }
+            requestBodyBuilder.deleteCharAt(requestBodyBuilder.length() - 1);
+            requestBody = requestBodyBuilder.toString();
+        }
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public enum RequestType {
+        GET,
+        POST,
+        PUT
+    }
+
 
 }
