565c38ed25c8cc70df5d0dc4411ea2bde7731104
Styro457 <raduursache6@gmail.com>
2024-03-26 00:58:11 +0200

    Remove unused class

--- src/main/java/com/github/dreamdevelopments/dreamapi/ui/elements/CustomInventory.java
+++
@@ -1,7 +0,0 @@
-package com.github.dreamdevelopments.dreamapi.ui.elements;
-
-import com.github.dreamdevelopments.dreamapi.effects.CustomSound;
-import org.bukkit.inventory.ItemStack;
-
-public record CustomInventory(int size, String title, ItemStack[] contents, CustomSound openSound, CustomSound clickSound) {
-}
