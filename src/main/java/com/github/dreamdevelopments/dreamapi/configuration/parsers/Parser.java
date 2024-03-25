9138ebdac9be598dffaedf07700ae744182e5698
Styro457 <raduursache6@gmail.com>
2024-03-25 17:12:37 +0200

    Add saveToConfig function in Parser

--- src/main/java/com/github/dreamdevelopments/dreamapi/configuration/parsers/Parser.java
+++ src/main/java/com/github/dreamdevelopments/dreamapi/configuration/parsers/Parser.java
@@ -1,10 +1,12 @@
 package com.github.dreamdevelopments.dreamapi.configuration.parsers;
 
 import com.github.dreamdevelopments.dreamapi.configuration.Config;
+import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 
+@Getter
 public abstract class Parser<T> {
 
     private static final HashMap<Class<?>, Parser<?>> parsers = new HashMap<>();
@@ -17,10 +19,19 @@ public abstract class Parser<T> {
         return parsers.containsKey(clazz);
     }
 
+    private final Class<T> classType;
+
     public Parser(@NotNull Class<T> classType) {
         parsers.put(classType, this);
+        this.classType = classType;
     }
 
     public abstract T loadFromConfig(@NotNull Config config, @NotNull String path);
+
+    public abstract void saveToConfig(@NotNull Config config, @NotNull String path, @NotNull T value);
+
+    public void saveObjectToConfig(@NotNull Config config, @NotNull String path, @NotNull Object value) {
+        saveToConfig(config, path, classType.cast(value));
+    }
 
 }
