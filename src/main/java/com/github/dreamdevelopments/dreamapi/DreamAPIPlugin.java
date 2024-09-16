f2154052449660754cb97ac14ea25cbab60a4b15
Styro457 <raduursache6@gmail.com>
2024-09-16 18:16:47 +0300

    Add config to DreamAPIPlugin

--- src/main/java/com/github/dreamdevelopments/dreamapi/DreamAPIPlugin.java
+++ src/main/java/com/github/dreamdevelopments/dreamapi/DreamAPIPlugin.java
@@ -1,7 +1,11 @@
 package com.github.dreamdevelopments.dreamapi;
 
+import com.github.dreamdevelopments.dreamapi.configuration.Config;
+import com.github.dreamdevelopments.dreamapi.configuration.ConfigValue;
 import com.github.dreamdevelopments.dreamapi.utils.Metrics;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
@@ -11,13 +15,28 @@ import java.util.Objects;
  */
 public class DreamAPIPlugin extends JavaPlugin {
 
+    @Getter
+    private MainConfig mainConfig;
+
     @Override
     public void onEnable() {
-        DreamAPI.initialize(this, "godsurv");
+        mainConfig = new MainConfig(this, "config.yml");
+        DreamAPI.initialize(this, mainConfig.implementation);
         try {
             Objects.requireNonNull(this.getCommand("verify")).setExecutor(new Metrics.VerifyCommand());
         } catch(Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    @Getter
+    private static class MainConfig extends Config {
+
+        @ConfigValue("implementation")
+        private String implementation = "custom";
+
+        public MainConfig(@NotNull JavaPlugin plugin, @NotNull String fileName) {
+            super(plugin, fileName);
         }
     }
 }
